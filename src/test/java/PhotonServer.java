b26ec8f9c31aa9ea5c25ef0153ddbc6412c7e8cd
mo2c <u12004589@tuks.co.za>
2016-07-05 12:53:02 +0200

    Added MQTTBroker dependency and Config file.

--- src/test/java/PhotonServer.java
+++ src/test/java/PhotonServer.java
@@ -2,14 +2,12 @@
  * Created by Stuart on 2016/06/26.
  */
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Enumeration;
-import java.util.Hashtable;
 import java.util.LinkedList;
+import io.moquette.*;
+import io.moquette.server.Server;
+
 
 public class PhotonServer
 {
@@ -17,61 +15,16 @@ public class PhotonServer
 
     private LinkedList<String> list;
 
-    public void startServer( )
-    {
-
-    }
-
     public PhotonServer(int portNum) throws IOException
     {
-        listen(portNum);
+        Server server = new Server();
+        File configFile = new File("test.conf");
+        server.startServer(configFile);
     }
 
     public void listen(int port) throws IOException
     {
-        ServerSocket socket = new ServerSocket(port);
 
-        // Tell the world we're ready to go
-        System.out.println("Listening on " + socket);
-        // Keep accepting connections forever
-        while(true)
-        {
-            Socket s = socket.accept();
-            System.out.println( "Connection from "+s );
-            DataOutputStream dout = new DataOutputStream( s.getOutputStream() );
-            DataInputStream din = new DataInputStream(s.getInputStream());
-            String user = din.readUTF();
-            System.out.println("New user is " + user);
-
-            synchronized(this)
-            {
-               // outputStreams.put(s, dout);
-               // clients.put(dout, user);
-            }
-
-            new ServerThread( this, s );
-        }
-    }
-
-
-
-    public void removeConnection(Socket s)
-    {
-       /* synchronized(outputStreams)
-        {
-            System.out.println("Removing connection to " + s);
-            outputStreams.remove(s);
-
-            try
-            {
-                s.close();
-            }
-            catch(IOException ie)
-            {
-                System.out.println("Error closing " + s);
-                ie.printStackTrace();
-            }
-        }*/
     }
 
     public static void main(String [] args) throws IOException
--- src/test/java/ServerThread.java
+++ src/test/java/ServerThread.java
@@ -1,17 +1,25 @@
+
 /**
  * Created by Stuart on 2016/06/26.
  */
 
-import java.net.*;
-
-import persistence.Firebase.*;
 import persistence.PersistenceHandler.*;
+import persistence.Firebase.*;
+import io.moquette.server.*;
+
+
+import javax.inject.*;
+import java.net.Socket;
 
 public class ServerThread extends Thread
 {
     private PhotonServer photonServer;
+    @Inject
     private PersistenceHandler persistenceHandler;
+    @Inject
     private Socket socket;
+
+
     public ServerThread(PhotonServer photonServer, Socket socket)
     {
         this.photonServer = photonServer;

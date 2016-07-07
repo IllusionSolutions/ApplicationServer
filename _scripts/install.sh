6120ec527bc9acfd053dce8399b55880e2dbc73c
MothusiMasibi <mothusi.masibi@gmail.com>
2016-07-07 11:18:19 +0200

    Fixed issue with Known Hosts.

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,11 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_b06319473f3e_key -iv $encrypted_b06319473f3e_key -in id_rsa.enc -out id_rsa -d
-rm id_rsa.enc # Don't need it anymore
-chmod 600 id_rsa
-mv id_rsa ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_b06319473f3e_key -iv $encrypted_b06319473f3e_iv -in id_rsa.enc -out id_rsa -d
+eval "$(ssh-agent -s)"
+chmod 600 $TRAVIS_BUILD_DIR/id_rsa
+ssh-add $TRAVIS_BUILD_DIR/id_rsa
+
+#rm id_rsa.enc # Don't need it anymore
+#chmod 600 id_rsa
+#mv id_rsa ~/.ssh/id_rsa

e525fdef0cc70ad71f4827fdd10afc83c60fc271
Brandon Wardley <u29005150@tuks.co.za>
2016-07-13 13:25:41 +0200

    Added to StoreObject.java
    Added more members to StoreObject, as well as getters and setters for
    the necessary members.

--- src/main/java/persistence/PersistenceHandler/StoreObject.java
+++ src/main/java/persistence/PersistenceHandler/StoreObject.java
@@ -6,6 +6,70 @@ package persistence.PersistenceHandler;
 
 public class StoreObject
 {
-	public String id;
-	public double power;
+	private String id;
+	private double current;
+	private double voltage;
+	private double truePower;
+	private double reactivePower;
+	private double apparentPower;
+
+	public String getId()
+	{
+		return this.id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public double getCurrent()
+	{
+		return this.current;
+	}
+
+	public void setCurrent(double current)
+	{
+		this.current = current;
+	}
+
+	public double getVoltage()
+	{
+		return this.voltage;
+	}
+
+	public void setVoltage(double voltage)
+	{
+		this.voltage = voltage;
+	}
+
+	public double getTruePower()
+	{
+		return this.truePower;
+	}
+
+	public void setTruePower(double truePower)
+	{
+		this.truePower = truePower;
+	}
+
+	public double getReactivePower()
+	{
+		return this.reactivePower;
+	}
+
+	public void setReactivePower(double reactivePower)
+	{
+		this.reactivePower = reactivePower;
+	}
+
+	public double getApparentPower()
+	{
+		return this.apparentPower;
+	}
+
+	public void setApparentPower(double apparentPower)
+	{
+		this.apparentPower = apparentPower;
+	}
 }

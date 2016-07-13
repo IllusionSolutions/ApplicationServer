e3b0bd3ace278eb65953fc04844303fce0313d9c
Stuart Andrews <u12153983@tuks.co.za>
2016-07-13 13:05:14 +0200

    retrieve(InterceptPublishMessage message) function, originally toJSON(String payload) function

--- src/main/java/PublisherListener.java
+++ src/main/java/PublisherListener.java
@@ -1,19 +1,24 @@
 import io.moquette.interception.AbstractInterceptHandler;
 import io.moquette.interception.messages.InterceptPublishMessage;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.omg.CORBA.Object;
 import persistence.PersistenceHandler.PersistenceHandler;
+import org.json.simple.JSONObject;
 
-/**
- * Created by MothusiMasibi on 2016/07/05.
- */
+import java.util.Hashtable;
 
-public class PublisherListener extends AbstractInterceptHandler
+class PublisherListener extends AbstractInterceptHandler
 {
+    private PersistenceHandler persistenceHandler;
+    private String id = "";
+    private String payload = "";
+    private String array[];
 
-    private PersistenceHandler persistence;
-
-    public PublisherListener(PersistenceHandler persistence)
+    public PublisherListener(PersistenceHandler handler)
     {
-        this.persistence = persistence;
+        super();
+        persistenceHandler = handler;
     }
 
     @Override
@@ -24,7 +29,24 @@ public class PublisherListener extends AbstractInterceptHandler
                 "\nTopic: " + message.getTopicName() +
                 "\nMessage: " + new String(message.getPayload().array())
         );
-
     }
 
+    /** Takes in the InterceptPublishMessage message, and seperates the Photons ID
+     * from the payload, namely the Current, Voltage and Power, which
+     * are all stored in a JSON String. The retrieve function returns a String array
+     * that stores the ID and the payload JSON String.
+     *
+     * @param message			The data we receive from the Photon
+     * @return			        An array that stores the Photons ID and the Data we received
+     */
+    private String[] retrieve(InterceptPublishMessage message)
+    {
+        array = new String[2];
+        id = message.getClientID();
+        payload = new String(message.getPayload().array());
+        array[0] = id;
+        array[1] = payload;
+
+        return array;
+    }
 }

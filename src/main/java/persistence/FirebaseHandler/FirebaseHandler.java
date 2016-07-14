bb9f60bf44383038ba1244c1a36469e2db022f3b
Stuart Andrews <u12153983@tuks.co.za>
2016-07-14 13:36:56 +0200

    The store function obtains Authorization from firebase, and creates the reference to Firebase and determines the storage URL per StoreObject. Changed valideateID's return type to Integer.

--- src/main/java/persistence/FirebaseHandler/FirebaseHandler.java
+++ src/main/java/persistence/FirebaseHandler/FirebaseHandler.java
@@ -4,27 +4,30 @@
 
 package persistence.FirebaseHandler;
 
-import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import persistence.PersistenceHandler.PersistenceHandler;
 import persistence.PersistenceHandler.StoreObject;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Date;
 
 public class FirebaseHandler implements PersistenceHandler
 {
 	private StoreObject storeObject;
-	
+
 	private String APIKey;
 
 	private String URL;
 
-	Firebase ref;
+	private String [] months = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
 
-	String day;
+	private DatabaseReference powerCloudRef;
 
-	String month;
-
-	String year;
+	private FirebaseDatabase powerCloud;
 
 	public FirebaseHandler()
 	{
@@ -35,13 +38,11 @@ public class FirebaseHandler implements PersistenceHandler
 	 *
 	 * @param APIKey			The FirebaseHandler APIKey
 	 * @param URL				The FirebaseHandler URL
-	 * @return			        An array that stores the Photons ID and the Data we received
 	 */
 	public FirebaseHandler(String APIKey, String URL)
 	{
 		this.APIKey = APIKey;
 		this.URL = URL;
-		ref = new Firebase(this.URL);
 	}
 
 	public String getAPIKey()
@@ -49,7 +50,7 @@ public class FirebaseHandler implements PersistenceHandler
 		return APIKey;
 	}
 
-	public String getURL()
+	private String getURL()
 	{
 		return URL;
 	}
@@ -58,17 +59,81 @@ public class FirebaseHandler implements PersistenceHandler
 	{
 		APIKey = apiKey;
 	}
-	
+
+	/** Takes in a StoreObject object, and gets the authorization to edit Firebase.
+	 * It also determines the reference to the location where the data will be stored
+	 * within Firebase
+	 *
+	 * @param data			StoreObject object, containing the data that needs to be stored.
+	 * @return			    If the store is successful it returns true, else it returns false.
+	 */
 	public boolean store( StoreObject data )
 	{
-		Date date = new Date();
-		String day = "", month = "", year = "";
+		String day;
+		String month;
+		String year;
+		int device;
+		int month_int;
+		int day_int;
 
+		// Initialize the app with a service account, granting admin privileges
+		try
+		{
+			FirebaseOptions options;
+			options = new FirebaseOptions.Builder()
+                    .setDatabaseUrl(this.getURL())
+                    .setServiceAccount(new FileInputStream("authentication_configuration.json"))
+                    .build();
+			FirebaseApp.initializeApp(options);
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+
+		device = 0;
+		day = "";
+		month = "";
+		year = "";
+
+		/*Splitting the date into:
+			Day: eg 14
+			Month: eg Jul
+			Year: eg 2016
+
+		  From:
+		  	on Jul 14 09:51:52 CAT 2016
+		 */
+		Date date = new Date();
+		device = validateId(storeObject.getId());
 		day = date.toString().substring(4,7);
 		month = date.toString().substring(8,10);
 		year = date.toString().substring(24,28);
 
+		day_int = Integer.parseInt(day);
+		day_int -= 1;
+		month_int = checkMonth(month);
+
+		//Creating the Firebase reference
+		powerCloud = FirebaseDatabase.getInstance();
+		powerCloudRef = powerCloud.getReference(device + "/data/" + year + "/" + month_int + "/" + day_int);
+
 		return false;
+	}
+
+	/** Takes in a month string, and determines the month's index.
+	 *
+	 * @param month			Current month
+	 * @return			    The index, if the month isn't valid it returns -1
+	 */
+	private int checkMonth(String month)
+	{
+		for(int i = 0 ; i < 12 ; i++)
+		{
+			if(months[i].equals(month))
+				return i;
+		}
+		return  -1;
 	}
 
 	/** The validateId method, takes in one Strings.
@@ -76,9 +141,9 @@ public class FirebaseHandler implements PersistenceHandler
 	 * @param data			Data(id) to be check if it is in Firebase
 	 * @return			    True if id is in firebase, false if it isn't
 	 */
-	public boolean validateId(String data)
+	private int validateId(String data)
 	{
-		String newURL = URL+"/devices/0/id";
+		/*String newURL = URL+"/devices/0/id";
 		Firebase ref = new Firebase(newURL);
 
 		ref.child(data).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -94,6 +159,7 @@ public class FirebaseHandler implements PersistenceHandler
 
 			@Override
 			public void onCancelled(FirebaseError firebaseError) { }
-		});
+		});*/
+		return -1;
 	}
 }

bab8c6a521f21a3ca80c5715b1b62fceadaaa3cf
Stuart Andrews <u12153983@tuks.co.za>
2016-07-14 12:30:40 +0200

    FirebaseHandler.java firebase authentication and reference.

--- src/main/java/persistence/FirebaseHandler/FirebaseHandler.java
+++ src/main/java/persistence/FirebaseHandler/FirebaseHandler.java
@@ -4,10 +4,15 @@
 
 package persistence.FirebaseHandler;
 
-import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import persistence.PersistenceHandler.PersistenceHandler;
 import persistence.PersistenceHandler.StoreObject;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Date;
 
 public class FirebaseHandler implements PersistenceHandler
@@ -18,13 +23,15 @@ public class FirebaseHandler implements PersistenceHandler
 
 	private String URL;
 
-	Firebase ref;
-
 	String day;
 
 	String month;
 
 	String year;
+
+	private DatabaseReference powerCloudRef;
+
+	private FirebaseDatabase powerCloud;
 
 	public FirebaseHandler()
 	{
@@ -35,13 +42,11 @@ public class FirebaseHandler implements PersistenceHandler
 	 *
 	 * @param APIKey			The FirebaseHandler APIKey
 	 * @param URL				The FirebaseHandler URL
-	 * @return			        An array that stores the Photons ID and the Data we received
 	 */
 	public FirebaseHandler(String APIKey, String URL)
 	{
 		this.APIKey = APIKey;
 		this.URL = URL;
-		ref = new Firebase(this.URL);
 	}
 
 	public String getAPIKey()
@@ -49,7 +54,7 @@ public class FirebaseHandler implements PersistenceHandler
 		return APIKey;
 	}
 
-	public String getURL()
+	private String getURL()
 	{
 		return URL;
 	}
@@ -58,9 +63,29 @@ public class FirebaseHandler implements PersistenceHandler
 	{
 		APIKey = apiKey;
 	}
-	
+
 	public boolean store( StoreObject data )
 	{
+		// Initialize the app with a service account, granting admin privileges
+
+		try
+		{
+			FirebaseOptions options = null;
+			options = new FirebaseOptions.Builder()
+                    .setDatabaseUrl(this.getURL())
+                    .setServiceAccount(new FileInputStream("authentication_configuration.json"))
+                    .build();
+			FirebaseApp.initializeApp(options);
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+
+
+		powerCloud = FirebaseDatabase.getInstance();
+		powerCloudRef = powerCloud.getReference("device/year/month/day");
+
 		Date date = new Date();
 		String day = "", month = "", year = "";
 
@@ -70,4 +95,5 @@ public class FirebaseHandler implements PersistenceHandler
 
 		return false;
 	}
+
 }
